d051accd562b81e4ae36a5533b07fe2b97e7f3a3
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-05-14 15:28:47 +1000

    testing Size in non tty env
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- test/unit/size.spec.js
+++ test/unit/size.spec.js
@@ -10,6 +10,7 @@ const Size = CFonts.__test__.Size;
 
 
 test(`Size - Should have two keys with numbers`, () => {
+	console.log(Size);
 	expect( typeof Size.width ).toEqual( 'number' );
 	expect( typeof Size.height ).toEqual( 'number' );
 });
