5c964d0bc3cb1b68526083afd3f6b0cccf8f7bfc
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-05-14 15:36:54 +1000

    Fixed size detection in non-tty environments
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- test/unit/size.spec.js
+++ test/unit/size.spec.js
@@ -10,7 +10,11 @@ const Size = CFonts.__test__.Size;
 
 
 test(`Size - Should have two keys with numbers`, () => {
-	console.log(Size);
 	expect( typeof Size.width ).toEqual( 'number' );
 	expect( typeof Size.height ).toEqual( 'number' );
+});
+
+test(`Size - Should have only sizes that are larger than 0`, () => {
+	expect( Size.width > 0 ).toBe( true );
+	expect( Size.height > 0 ).toBe( true );
 });
