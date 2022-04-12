e3c66590e80d73e6fd8f1c15b6871e75da8ba66c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-04-12 11:46:00 +1000

    removed build artefacts

--- rust/target/debug/cfonts-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/dominik/Sites/cfonts/rust/target/debug/cfonts-rust: /Users/dominik/Sites/cfonts/rust/src/main.rs
--- rust/target/debug/deps/cfonts_rust-91dc1b4697ccd8cd.d
+++
@@ -1,7 +0,0 @@
-/Users/dominik/Sites/cfonts/rust/target/debug/deps/cfonts_rust-91dc1b4697ccd8cd.rmeta: src/main.rs
-
-/Users/dominik/Sites/cfonts/rust/target/debug/deps/cfonts_rust-91dc1b4697ccd8cd.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
