573a2f7b9f66dfa914b3d78038bd285bac7eeda8
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-04-17 22:13:41 +1000

    added boolean flags and flag stacks

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,7 +8,8 @@ pub mod helpers;
 
 use config::{Align, BgColors, CliOption, Colors, Env, Fonts, OptionType, Options, CLIOPTIONS};
 
-fn parse_args(args: &Vec<String>) -> Options {
+fn parse_args(args: Vec<String>) -> Options {
+	let mut my_args = args.clone();
 	let mut options = Options::default();
 
 	// create a lookup table for our CLIOPTIONS
@@ -20,18 +21,31 @@ fn parse_args(args: &Vec<String>) -> Options {
 	}
 
 	// our text to be converted
-	options.text = args[1].clone();
+	options.text = my_args[1].clone();
 
+	let mut args_length = my_args.len();
+	let mut i = 2;
 	// we iterate over all arguments and match them with our lookup table
-	for mut i in 2..args.len() {
-		match options_lookup.get(&args[i]) {
+	while i < args_length {
+		// before we see if this flag exists in our lookup we see if boolean flags have been stacked here
+		if my_args[i].starts_with("-") && !my_args[i].starts_with("--") && my_args[i].len() > 2 {
+			// we know that this is a stack of boolean flags
+			let this_flag = my_args[i].clone();
+			// unwrap is guarded by if clause it's contained in
+			for flag in this_flag.strip_prefix("-").unwrap().chars() {
+				my_args.push(format!("-{}", flag));
+				args_length += 1;
+			}
+		}
+
+		match options_lookup.get(&my_args[i]) {
 			Some(this_flag) => {
 				println!("found {:?}", this_flag.kind);
 				match this_flag.kind {
 					OptionType::Text => { /* Only Text type is on argvs[1] */ }
 					OptionType::Font => {
 						i += 1;
-						options.font = match args[i].to_lowercase().as_str() {
+						options.font = match my_args[i].to_lowercase().as_str() {
 							"console" => Fonts::FontConsole,
 							"block" => Fonts::FontBlock,
 							"simpleblock" => Fonts::FontSimpleBlock,
@@ -53,7 +67,7 @@ fn parse_args(args: &Vec<String>) -> Options {
 					}
 					OptionType::Align => {
 						i += 1;
-						options.align = match args[i].to_lowercase().as_str() {
+						options.align = match my_args[i].to_lowercase().as_str() {
 							"left" => Align::Left,
 							"center" => Align::Center,
 							"right" => Align::Right,
@@ -72,18 +86,38 @@ fn parse_args(args: &Vec<String>) -> Options {
 					OptionType::Colors => {}
 					OptionType::Color => {}
 					OptionType::Number => {}
-					OptionType::Bool => {}
+					OptionType::Bool => match this_flag.key {
+						"spaceless" => {
+							options.spaceless = true;
+						}
+						"independent_gradient" => {
+							options.independent_gradient = true;
+						}
+						"transition_gradient" => {
+							options.transition_gradient = true;
+						}
+						"version" => {
+							options.version = true;
+						}
+						"debug" => {
+							options.debug = true;
+						}
+						_ => {}
+					},
 					OptionType::Env => {}
 				}
 				// if this_flag.getter.len() > 0 {
 				// 	println!("getter {:?}", this_flag);
 				// 	// i += 1;
-				// 	// let value = this_flag.get(&args[i]);
+				// 	// let value = this_flag.get(&my_args[i]);
 				// 	// println!("value {:?}", value);
 				// }
 			}
 			None => { /* (TODO: debug message) We ignore flags we don't recognize */ }
 		};
+
+		// iterating to next loop count
+		i += 1;
 	}
 
 	options
@@ -98,7 +132,7 @@ fn main() {
 	// 	..Options::default()
 	// };
 
-	let options = parse_args(&args().collect());
+	let options = parse_args(args().collect());
 	println!("{:#?}", options);
 
 	println!("Hello, world!");
