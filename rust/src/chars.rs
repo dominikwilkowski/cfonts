3af3f0328537278ee21ad23837e674b582143f95
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-05-09 11:08:05 -0400

    added chars::get_longest_line_len

--- rust/src/chars.rs
+++ rust/src/chars.rs
@@ -29,14 +29,6 @@ pub fn get_letter_space(letter_space: &[String], options: &Options) -> Vec<Strin
 	output
 }
 
-// pub fn get_longest_line_len(output: &Vec<String>, font_lines: &usize, options: &Options) -> usize {}
-
-// pub fn get_char_length(char: &Vec<String>, options: &Options) -> usize {}
-
-// pub fn paint_char(char: &Vec<String>, colors: &Vec<[String; 2]>, options: &Options) -> Vec<String> {}
-
-// pub fn add_char(char: &Vec<String>, output: &Vec<String>, options: &Options) -> Vec<String> {}
-
 pub fn add_line(output: &mut Vec<String>, font_lines: usize, options: &Options) -> Vec<String> {
 	d("chars::add_line()", 2, Dt::Head, options, &mut std::io::stdout());
 	d(
@@ -54,5 +46,58 @@ pub fn add_line(output: &mut Vec<String>, font_lines: usize, options: &Options) 
 	d(&format!("chars::add_line() -> {:?}", output), 2, Dt::Log, options, &mut std::io::stdout());
 	output.to_vec()
 }
+
+// pub fn add_letter(char: &Vec<String>, output: &Vec<String>, options: &Options) -> Vec<String> {}
+
+pub fn get_longest_line_len(output: &[String], font_lines: usize, options: &Options) -> usize {
+	d("chars::get_longest_line_len()", 2, Dt::Head, options, &mut std::io::stdout());
+	d(
+		&format!("chars::get_longest_line_len()\noutput:{:?}\nfont_lines:{:?}", output, font_lines),
+		2,
+		Dt::Log,
+		options,
+		&mut std::io::stdout(),
+	);
+
+	let size = output.iter().rev().take(font_lines).fold(0, |acc, item| if item.len() > acc { item.len() } else { acc });
+
+	d(&format!("chars::get_longest_line_len() -> {:?}", size), 2, Dt::Log, options, &mut std::io::stdout());
+	size
+}
+
+pub fn get_letter_length(letter: &[String], font_colors: usize, options: &Options) -> usize {
+	d("chars::get_char_length()", 2, Dt::Head, options, &mut std::io::stdout());
+	d(
+		&format!("chars::get_char_length()\nchar:{:?}\nfont_colors:{:?}", letter, font_colors),
+		2,
+		Dt::Log,
+		options,
+		&mut std::io::stdout(),
+	);
+
+	let size = letter.iter().fold(0, |acc, item| {
+		let mut stripped_item = item.clone();
+		// first we remove color annotations
+		// but fonts that support only a single color don't have color annotations
+		if font_colors > 1 {
+			for i in 1..=font_colors {
+				let open = format!("<c{}>", i);
+				let close = format!("</c{}>", i);
+				stripped_item = stripped_item.replace(&open, "").replace(&close, "");
+			}
+		}
+
+		if stripped_item.len() > acc {
+			stripped_item.len()
+		} else {
+			acc
+		}
+	});
+
+	d(&format!("chars::get_char_length() -> {:?}", size), 2, Dt::Log, options, &mut std::io::stdout());
+	size
+}
+
+// pub fn paint_letter(char: &Vec<String>, colors: &Vec<[String; 2]>, options: &Options) -> Vec<String> {}
 
 // pub fn align_last_line(output: &Vec<String>, font_lines: &usize, options: &Options) -> Vec<String> {}
--- rust/tests/char_test.rs
+++ rust/tests/char_test.rs
@@ -1,6 +1,6 @@
 extern crate cfonts;
 
-use cfonts::chars::{add_line, get_letter_space};
+use cfonts::chars::{add_line, get_letter_length, get_letter_space, get_longest_line_len};
 use cfonts::config::Options;
 
 #[cfg(test)]
@@ -72,5 +72,62 @@ mod chars {
 				String::new(),
 			]
 		);
+	}
+
+	//
+
+	#[test]
+	fn get_longest_line_len_works() {
+		let options = Options::default();
+		let mut output = vec![
+			String::from("1"),
+			String::from("1"),
+			String::from("2"),
+			String::from("2"),
+			String::from("3333"),
+			String::from("3"),
+		];
+		assert_eq!(get_longest_line_len(&mut output, 2, &options), 4);
+
+		output = vec![
+			String::from("1"),
+			String::from("1"),
+			String::from("1"),
+			String::from("222"),
+			String::from("22"),
+			String::from("22"),
+		];
+		assert_eq!(get_longest_line_len(&mut output, 3, &options), 3);
+
+		output = vec![
+			String::from("1"),
+			String::from("1"),
+			String::from("2"),
+			String::from("2"),
+			String::from("3333"),
+			String::from("33333"),
+		];
+		assert_eq!(get_longest_line_len(&mut output, 2, &options), 5);
+	}
+
+	#[test]
+	fn get_letter_length_works() {
+		let options = Options::default();
+		let mut letter = vec![
+			String::from("1<c1>1</c1><c2>1</c2>"),
+			String::from("1<c1>1</c1><c1>1</c1>"),
+			String::from("1"),
+		];
+		assert_eq!(get_letter_length(&letter, 2, &options), 3);
+
+		letter = vec![
+			String::from("1<c1>1</c1><c2>1</c2>"),
+			String::from("1<c1>1</c1><c1>1</c1><c3>1</c3>"),
+			String::from("1"),
+		];
+		assert_eq!(get_letter_length(&letter, 3, &options), 4);
+
+		letter = vec![String::from("111"), String::from("1111"), String::from("11111")];
+		assert_eq!(get_letter_length(&letter, 1, &options), 5);
 	}
 }
