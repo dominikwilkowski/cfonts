49240525f7849fda1e812183e0d5dd41c4e96dbf
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-09-05 00:32:01 +1000

    fixed up simpleBlock font height

--- crates/cfonts/src/fonts/simple_block.rs
+++ crates/cfonts/src/fonts/simple_block.rs
@@ -1,7 +1,7 @@
 use crate::fonts::{FontFile, Glyph, GlyphRow, Segment};
 use cfonts_macros::glyph;
 
-pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
+pub static FONT_SIMPLE_BLOCK: FontFile<5> = FontFile {
 	name: "simpleBlock",
 	#[rustfmt::skip]
 	buffer_start: &[
@@ -10,13 +10,9 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 		GlyphRow { segments: &[Segment::Plain("")] },
 		GlyphRow { segments: &[Segment::Plain("")] },
 		GlyphRow { segments: &[Segment::Plain("")] },
-		GlyphRow { segments: &[Segment::Plain("")] },
-		GlyphRow { segments: &[Segment::Plain("")] },
 	],
 	#[rustfmt::skip]
 	buffer_end: &[
-		GlyphRow { segments: &[Segment::Plain("")] },
-		GlyphRow { segments: &[Segment::Plain("")] },
 		GlyphRow { segments: &[Segment::Plain("")] },
 		GlyphRow { segments: &[Segment::Plain("")] },
 		GlyphRow { segments: &[Segment::Plain("")] },
@@ -31,8 +27,6 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 		r"  ",
 		r"  ",
 		r"  ",
-		r"  ",
-		r"  ",
 	),
 	colors: 1,
 	line_height: 1,
@@ -40,371 +34,289 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 	glyphs: {
 		let mut table = [None; 128];
 		table['A' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"_|_|_|_|",
 			r"_|    _|",
 			r"_|    _|",
-			r"        ",
 		));
 		table['B' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['C' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|_|",
 			r"_|      ",
 			r"_|      ",
 			r"_|      ",
 			r"  _|_|_|",
-			r"        ",
 		));
 		table['D' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['E' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|_|",
 			r"_|      ",
 			r"_|_|_|  ",
 			r"_|      ",
 			r"_|_|_|_|",
-			r"        ",
 		));
 		table['F' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|_|",
 			r"_|      ",
 			r"_|_|_|  ",
 			r"_|      ",
 			r"_|      ",
-			r"        ",
 		));
 		table['G' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|_|",
 			r"_|      ",
 			r"_|  _|_|",
 			r"_|    _|",
 			r"  _|_|_|",
-			r"        ",
 		));
 		table['H' as usize] = Some(glyph!(
-			r"        ",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|_|_|_|",
 			r"_|    _|",
 			r"_|    _|",
-			r"        ",
 		));
 		table['I' as usize] = Some(glyph!(
-			r"      ",
 			r"_|_|_|",
 			r"  _|  ",
 			r"  _|  ",
 			r"  _|  ",
 			r"_|_|_|",
-			r"      ",
 		));
 		table['J' as usize] = Some(glyph!(
-			r"        ",
 			r"      _|",
 			r"      _|",
 			r"      _|",
 			r"_|    _|",
 			r"  _|_|  ",
-			r"        ",
 		));
 		table['K' as usize] = Some(glyph!(
-			r"        ",
 			r"_|    _|",
 			r"_|  _|  ",
 			r"_|_|    ",
 			r"_|  _|  ",
 			r"_|    _|",
-			r"        ",
 		));
 		table['L' as usize] = Some(glyph!(
-			r"        ",
 			r"_|      ",
 			r"_|      ",
 			r"_|      ",
 			r"_|      ",
 			r"_|_|_|_|",
-			r"        ",
 		));
 		table['M' as usize] = Some(glyph!(
-			r"          ",
 			r"_|      _|",
 			r"_|_|  _|_|",
 			r"_|  _|  _|",
 			r"_|      _|",
 			r"_|      _|",
-			r"          ",
 		));
 		table['N' as usize] = Some(glyph!(
-			r"          ",
 			r"_|      _|",
 			r"_|_|    _|",
 			r"_|  _|  _|",
 			r"_|    _|_|",
 			r"_|      _|",
-			r"          ",
 		));
 		table['O' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|    _|",
 			r"  _|_|  ",
-			r"        ",
 		));
 		table['P' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|_|_|  ",
 			r"_|      ",
 			r"_|      ",
-			r"        ",
 		));
 		table['Q' as usize] = Some(glyph!(
-			r"          ",
 			r"  _|_|    ",
 			r"_|    _|  ",
 			r"_|  _|_|  ",
 			r"_|    _|  ",
 			r"  _|_|  _|",
-			r"          ",
 		));
 		table['R' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"_|    _|",
-			r"        ",
 		));
 		table['S' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|_|",
 			r"_|      ",
 			r"  _|_|  ",
 			r"      _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['T' as usize] = Some(glyph!(
-			r"          ",
 			r"_|_|_|_|_|",
 			r"    _|    ",
 			r"    _|    ",
 			r"    _|    ",
 			r"    _|    ",
-			r"          ",
 		));
 		table['U' as usize] = Some(glyph!(
-			r"        ",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|    _|",
 			r"_|    _|",
 			r"  _|_|  ",
-			r"        ",
 		));
 		table['V' as usize] = Some(glyph!(
-			r"          ",
 			r"_|      _|",
 			r"_|      _|",
 			r"_|      _|",
 			r"  _|  _|  ",
 			r"    _|    ",
-			r"          ",
 		));
 		table['W' as usize] = Some(glyph!(
-			r"              ",
 			r"_|          _|",
 			r"_|          _|",
 			r"_|    _|    _|",
 			r"  _|  _|  _|  ",
 			r"    _|  _|    ",
-			r"              ",
 		));
 		table['X' as usize] = Some(glyph!(
-			r"          ",
 			r"_|      _|",
 			r"  _|  _|  ",
 			r"    _|    ",
 			r"  _|  _|  ",
 			r"_|      _|",
-			r"          ",
 		));
 		table['Y' as usize] = Some(glyph!(
-			r"          ",
 			r"_|      _|",
 			r"  _|  _|  ",
 			r"    _|    ",
 			r"    _|    ",
 			r"    _|    ",
-			r"          ",
 		));
 		table['Z' as usize] = Some(glyph!(
-			r"          ",
 			r"_|_|_|_|_|",
 			r"      _|  ",
 			r"    _|    ",
 			r"  _|      ",
 			r"_|_|_|_|_|",
-			r"          ",
 		));
 		table['0' as usize] = Some(glyph!(
-			r"      ",
 			r"  _|  ",
 			r"_|  _|",
 			r"_|  _|",
 			r"_|  _|",
 			r"  _|  ",
-			r"      ",
 		));
 		table['1' as usize] = Some(glyph!(
-			r"    ",
 			r"  _|",
 			r"_|_|",
 			r"  _|",
 			r"  _|",
 			r"  _|",
-			r"    ",
 		));
 		table['2' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"    _|  ",
 			r"  _|    ",
 			r"_|_|_|_|",
-			r"        ",
 		));
 		table['3' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|  ",
 			r"      _|",
 			r"  _|_|  ",
 			r"      _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['4' as usize] = Some(glyph!(
-			r"        ",
 			r"_|  _|  ",
 			r"_|  _|  ",
 			r"_|_|_|_|",
 			r"    _|  ",
 			r"    _|  ",
-			r"        ",
 		));
 		table['5' as usize] = Some(glyph!(
-			r"        ",
 			r"_|_|_|_|",
 			r"_|      ",
 			r"_|_|_|  ",
 			r"      _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['6' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|_|",
 			r"_|      ",
 			r"_|_|_|  ",
 			r"_|    _|",
 			r"  _|_|  ",
-			r"        ",
 		));
 		table['7' as usize] = Some(glyph!(
-			r"          ",
 			r"_|_|_|_|_|",
 			r"        _|",
 			r"      _|  ",
 			r"    _|    ",
 			r"  _|      ",
-			r"          ",
 		));
 		table['8' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"  _|_|  ",
-			r"        ",
 		));
 		table['9' as usize] = Some(glyph!(
-			r"        ",
 			r"  _|_|  ",
 			r"_|    _|",
 			r"  _|_|_|",
 			r"      _|",
 			r"_|_|_|  ",
-			r"        ",
 		));
 		table['!' as usize] = Some(glyph!(
-			r"  ",
 			r"_|",
 			r"_|",
 			r"_|",
 			r"  ",
 			r"_|",
-			r"  ",
 		));
 		table['?' as usize] = Some(glyph!(
-			r"      ",
 			r"_|_|  ",
 			r"    _|",
 			r"_|_|  ",
 			r"      ",
 			r"_|    ",
-			r"      ",
 		));
 		table['.' as usize] = Some(glyph!(
 			r"  ",
 			r"  ",
 			r"  ",
 			r"  ",
-			r"  ",
 			r"_|",
-			r"  ",
 		));
 		table['+' as usize] = Some(glyph!(
-			r"          ",
 			r"    _|    ",
 			r"    _|    ",
 			r"_|_|_|_|_|",
 			r"    _|    ",
 			r"    _|    ",
-			r"          ",
 		));
 		table['-' as usize] = Some(glyph!(
 			r"          ",
 			r"          ",
-			r"          ",
 			r"_|_|_|_|_|",
-			r"          ",
 			r"          ",
 			r"          ",
 		));
@@ -413,68 +325,52 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 			r"          ",
 			r"          ",
 			r"          ",
-			r"          ",
 			r"_|_|_|_|_|",
-			r"          ",
 		));
 		table['=' as usize] = Some(glyph!(
 			r"          ",
-			r"          ",
 			r"_|_|_|_|_|",
 			r"          ",
 			r"_|_|_|_|_|",
-			r"          ",
 			r"          ",
 		));
 		table['@' as usize] = Some(glyph!(
-			r"    _|_|_|_|_|  ",
-			r"  _|          _|",
-			r"_|    _|_|_|  _|",
-			r"_|  _|    _|  _|",
-			r"_|    _|_|_|_|  ",
-			r"  _|            ",
-			r"    _|_|_|_|_|_|",
+			r"  _|_|_|_|_|  ",
+			r"_|          _|",
+			r"_|  _|      _|",
+			r"_|  _|_|_|_|  ",
+			r"  _|_|_|_|_|_|",
 		));
 		table['#' as usize] = Some(glyph!(
-			r"          ",
 			r"  _|  _|  ",
 			r"_|_|_|_|_|",
 			r"  _|  _|  ",
 			r"_|_|_|_|_|",
 			r"  _|  _|  ",
-			r"          ",
 		));
 		table['$' as usize] = Some(glyph!(
-			r"  _|  ",
-			r"_|_|_|",
+			r"  _|_|",
 			r"_|_|  ",
 			r"  _|_|",
 			r"_|_|_|",
 			r"  _|  ",
-			r"      ",
 		));
 		table['%' as usize] = Some(glyph!(
-			r"          ",
 			r"_|_|    _|",
 			r"_|_|  _|  ",
 			r"    _|    ",
 			r"  _|  _|_|",
 			r"_|    _|_|",
-			r"          ",
 		));
 		table['&' as usize] = Some(glyph!(
-			r"          ",
 			r"  _|      ",
 			r"_|  _|    ",
 			r"  _|_|  _|",
 			r"_|    _|  ",
 			r"  _|_|  _|",
-			r"          ",
 		));
 		table['(' as usize] = Some(glyph!(
 			r"  _|",
-			r"_|  ",
-			r"_|  ",
 			r"_|  ",
 			r"_|  ",
 			r"_|  ",
@@ -485,31 +381,23 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 			r"  _|",
 			r"  _|",
 			r"  _|",
-			r"  _|",
-			r"  _|",
 			r"_|  ",
 		));
 		table['/' as usize] = Some(glyph!(
-			r"          ",
 			r"        _|",
 			r"      _|  ",
 			r"    _|    ",
 			r"  _|      ",
 			r"_|        ",
-			r"          ",
 		));
 		table[':' as usize] = Some(glyph!(
 			r"  ",
-			r"  ",
 			r"_|",
 			r"  ",
 			r"_|",
-			r"  ",
 			r"  ",
 		));
 		table[';' as usize] = Some(glyph!(
-			r"    ",
-			r"    ",
 			r"    ",
 			r"  _|",
 			r"    ",
@@ -517,8 +405,6 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 			r"_|  ",
 		));
 		table[',' as usize] = Some(glyph!(
-			r"    ",
-			r"    ",
 			r"    ",
 			r"    ",
 			r"    ",
@@ -531,8 +417,6 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 			r"  ",
 			r"  ",
 			r"  ",
-			r"  ",
-			r"  ",
 		));
 		table['"' as usize] = Some(glyph!(
 			r"_|_|",
@@ -540,12 +424,8 @@ pub static FONT_SIMPLE_BLOCK: FontFile<7> = FontFile {
 			r"    ",
 			r"    ",
 			r"    ",
-			r"    ",
-			r"    ",
 		));
 		table[' ' as usize] = Some(glyph!(
-			r"   ",
-			r"   ",
 			r"   ",
 			r"   ",
 			r"   ",
